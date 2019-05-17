9ef33feb18a8956b253f23d7ec0dae07fdb93b98
Lucas Daniel Borges de Oliveira <lukys_taylor@hotmail.com>
2019-05-17 14:40:39 -0300

    first commit

---
+++ src/main/java/com/example/poc/lowhighIntegratePlatform/LowhighIntegratePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.example.poc.lowhighIntegratePlatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowhighIntegratePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowhighIntegratePlatformApplication.class, args);
+	}
+
+}
